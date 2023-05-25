877133d9559efd0d2dce29b968cf2a03efff3e93
Dương Nguyễn Tấn Hòa <tanhoatm@gmail.com>
2023-05-25 09:45:47 +0700

    Added Features

---
+++ src/core/Payment.Application/Features/PaymentNotification/Queries/GetPaymentNotifications.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Application.Features.PaymentNotification.Queries
+{
+    internal class GetPaymentNotifications
+    {
+    }
+}
---
+++ src/core/Payment.Application/Features/PaymentSignature/Commands/UpdateSignature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Application.Features.PaymentSignature.Commands
+{
+    internal class UpdateSignature
+    {
+    }
+}
